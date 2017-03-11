8c3f7afe71ed887b952f736d9192905f7c009d1a
RiccardoAncarani <riccardoancarani94@gmail.com>
2017-03-11 19:41:06 +0100

    Move everything in functions

--- playstore-comments.py
+++ playstore-comments.py
@@ -2,21 +2,19 @@ import requests
 import re
 from nltk.corpus import stopwords
 from wordcloud import WordCloud
+import sys
 
 def getGooglePlayReviews(page):
-    headers = {
-        
+    headers = {        
         "CONSENT":"YES+IT.it+20160117-18-0",
-        
-    }
+        }
 
     data = {
         "reviewType": 0,
         "pageNum" :page,
         "id":"com.facebook.katana",
         "reviewSortOrder":4,
-        "xhr": 1,
-        "token": "WZoOkfYty7v0H9yjvbZZ1pPDe5Q%3A1486642693855",
+        "xhr": 1,        
         "hl":"it"
     }
     r = requests.post("https://play.google.com/store/getreviews?authuser=0", headers=headers, data=data)
@@ -31,14 +29,21 @@ def getNPages(n):
     s = [x[25:-24] for x in s]
     return s
 
-s = getNPages(5)
-stop = set(stopwords.words('italian'))
-final_list = [" ".join([i for i in x.lower().split() if i not in stop]) for x in s]
+
+
+
 def showWordCloud(wordcloud):
     import matplotlib.pyplot as plt
     plt.imshow(wordcloud)
     plt.axis("off")
     plt.show()
 
-wordcloud = WordCloud().generate(" ".join(final_list))
-showWordCloud(wordcloud)
+def showCloudFromList(final_list):
+    wordcloud = WordCloud().generate(" ".join(final_list))
+    showWordCloud(wordcloud)
+
+if __name__ == '__main__':
+    s = getNPages(int(sys.argv[1]))
+    stop = set(stopwords.words('italian'))
+    final_list = [" ".join([i for i in x.lower().split() if i not in stop]) for x in s]
+    showCloudFromList(final_list)
